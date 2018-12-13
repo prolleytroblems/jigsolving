91db3528ab81810dff2c97fa6541ce0fc9809333
Lucas <luknak@hotmail.com>
2018-12-13 16:46:49 +0900

    add filter functionality

--- piecefinder.py
+++ piecefinder.py
@@ -42,24 +42,44 @@ class PieceFinder(object):
 
 class BBoxFilter(object):
 
-    def __init__(self, edgewidth=1):
+    def __init__(self, edgewidth=1, **kwargs):
         self.width=edgewidth
+        self.configure(**kwargs)
 
-    def configure(expansion=2, borderwidth=4, border_to_grad=0.5):
+
+    def configure(expansion=2, borderwidth=4, border_to_grad=0.5, **kwargs):
         self.expansion=expansion
         self.borderwidth
         self.weights=(border_to_grad, 1-border_to_grad)
 
+
     def __call__(self, array, boxes):
-        pass
+        subarrays=list(map(lambda box: self.get_subarray(array, box, 2), boxes))
+        scores=list(map(lambda subarray: self.score_array(subarray), subarrays))
 
 
-    def check(self, array):
-        pass
+
+    def get_subarray(array, box, expansion=0):
+        corners=[box[1]-expansion, box[1]+box[3]+expansion,
+                 box[0]-expansion, box[0]+box[2]+expansion]
+        if corners[0]<0:
+            corners[0]=0
+        if corners[2]<0:
+            corners[2]=0
+
+        return array[corners[0]:corners[1], corners[2]:corners[3]]
+
+
+    def shrink_arrays(arrays, layers):
+        return list(map(lambda array: array[layers:array.shape[0]-layers,
+                                            layers:array.shape[1]-layers],
+                        arrays))
+
 
     def score_array(self, array):
-
-        score=border_score*self.weights[0]+grad_score*self.weights[1]
+        b_score=self.border_score(array)
+        c_score=self.contrast_score(array)
+        score=b_score*self.weights[0]+c_score*self.weights[1]
         return score
 
 
@@ -76,6 +96,7 @@ class BBoxFilter(object):
                 "S": extract_h(array, list(range(-1, -1-thiccness, -1))),
                 "W": extract_v(array, list(range(thiccness)))}
 
+
     def extract_all_borders(self, array, thiccness):
         if thiccness==1:
             axis=0
@@ -89,12 +110,14 @@ class BBoxFilter(object):
             np.concatenate((strip, next_strip), axis=axis)
         return strip
 
+
     def border_score(self, array, thiccness=1, color=np.array((255,255,255))):
         border = self.extract_all_borders(array, thiccness)
         avg=np.sum(border, axis=(0,1))/n
         square_error=np.sum((avg-color)**2)
         score=1/(square_error+1)
         return score
+
 
     def contrast_score(self, array, thiccness=2, exp_scaling=2, half_mark=10):
         assert thiccness>1
@@ -105,11 +128,11 @@ class BBoxFilter(object):
             base_value = np.sum(border[0], axis=0)
             subscore=0
             for i in range(thiccness-1):
-                #channel-wise difference of layer averages
+                #channel-wise difference of layer intensity averages
                 lsubscore=np.sum(border[i+1], axis=0)-base_value
                 #avg of absolute differences across channels
                 lsubscore=np.sum(np.absolute(subscore))/3
-                #scaling the value that gives score of 0.5
+                #scaling the intensity difference value that gives score of 0.5
                 lsubscore=subscore/half_mark
                 #sigmoid normalization
                 lsubscore=subscore/(1+subscore)
